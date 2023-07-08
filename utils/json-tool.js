ca854f34dfa70946656a460275cbf5d574a49875
Eddy Chang <eddy@joomla.com.tw>
2023-07-08 18:40:58 +0800

    add wirteJsonFile

--- utils/json-tool.js
+++ utils/json-tool.js
@@ -2,7 +2,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-import { readFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
 
 /**
  * Fetching data from the JSON file and parse to JS data
@@ -12,4 +12,20 @@ import { readFile } from 'fs/promises'
 export const readJsonFile = async (pathname) => {
   const data = await readFile(path.join(process.cwd(), pathname))
   return JSON.parse(data)
+}
+
+export const writeJsonFile = async (pathname, jsonOrObject) => {
+  try {
+    // we need string
+    const data =
+      typeof jsonOrObject === 'object'
+        ? JSON.stringify(jsonOrObject)
+        : jsonOrObject
+
+    await writeFile(path.join(process.cwd(), pathname), data)
+    return true
+  } catch (e) {
+    console.log(e)
+    return false
+  }
 }
