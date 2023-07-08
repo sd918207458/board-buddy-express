1d27e568bc7d9e3a5db24c9c1055ead60321a298
Eddy Chang <eddy@joomla.com.tw>
2023-07-08 18:59:22 +0800

    U

--- utils/json-tool.js
+++ utils/json-tool.js
@@ -1,7 +1,4 @@
 import path from 'path'
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 import { readFile, writeFile } from 'fs/promises'
 
 /**
