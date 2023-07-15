8e18afe8980878e1957eb2ae0cfb26b86ec85b9a
Eddy Chang <eddy@joomla.com.tw>
2023-07-15 15:59:42 +0800

    change otplib to otpauth

--- config/otp.js
+++ config/otp.js
@@ -1,25 +1,31 @@
-import { totp } from 'otplib'
+import * as OTPAuth from 'otpauth'
 import 'dotenv/config.js'
 
-const sharedSecret = process.env.OTP_SECRET
+const otpSecret = process.env.OTP_SECRET
 
-// https://github.com/yeojz/otplib#totp-options
-// totp.options = { step: 30 } // 30s step
+// global object
+let totp = null
 
-// generateToken
-// use email+sharedSecret for shared secret for each user
+// Generate a token (returns the current token as a string).
+// each user use email+sharedSecret for shared secret
 const generateToken = (email = '') => {
-  return totp.generate(email + sharedSecret)
+  // Create a new TOTP object.
+  totp = new OTPAuth.TOTP({
+    issuer: 'ACME',
+    label: 'AzureDiamond',
+    algorithm: 'SHA1',
+    digits: 6,
+    period: 30,
+    secret: OTPAuth.Secret.fromLatin1(email + otpSecret),
+  })
+
+  return totp.generate()
 }
 
-// verifyToken - only for step 30s token verify
+// Validate a token (returns the token delta or null if it is not found in the search window, in which case it should be considered invalid).
 const verifyToken = (token) => {
-  try {
-    return totp.verify({ token, sharedSecret })
-  } catch (err) {
-    console.error(err)
-    return false
-  }
+  const delta = totp.validate({ token, window: 1 })
+  return delta === null ? false : true
 }
 
 export { generateToken, verifyToken }
