500f82274e4eb045930a4bfc9774284feca9de5b
Eddy Chang <eddy@joomla.com.tw>
2023-07-20 10:13:42 +0800

    + toKebabCase func

--- utils/tool.js
+++ utils/tool.js
@@ -41,4 +41,11 @@ function isEmpty(obj) {
   /* eslint-enable  */
 }
 
-export { extendLog, isEmpty }
+const toKebabCase = (str) =>
+  str &&
+  str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map((x) => x.toLowerCase())
+    .join('-')
+
+export { extendLog, isEmpty, toKebabCase }
