9e98a1909865672d73c2dc0eafa0a551c71d1cd8
Eddy Chang <eddy@joomla.com.tw>
2023-07-30 16:48:48 +0800

    add DELETE to cors option

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const app = express()
 app.use(
   cors({
     origin: ['http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 )
