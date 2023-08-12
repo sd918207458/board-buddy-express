3465ac4baabd77ace5b71c4359b0ff6c2d676809
Eddy Chang <eddy@joomla.com.tw>
2023-08-12 18:38:38 +0800

    u

--- data/users_2023-08-12_174416.sql
+++ data/users_2023-08-12_174416.sql
@@ -31,17 +31,12 @@ CREATE TABLE `users` (
   `r_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `google_uid` varchar(200) DEFAULT NULL,
   `photo_url` varchar(200) DEFAULT NULL,
+  `line_uid` varchar(200) DEFAULT NULL,
+  `line_access_token` text,
+  `fb_uid` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'金妮','ginny@test.com','ginny132','12345','2023-01-01 00:00:00',NULL,NULL),(2,'哈利','herry@test.com','herry','22222','2023-06-03 00:00:00',NULL,NULL),(3,'妙麗','mione@test.com','mione','33333','2023-06-06 00:00:00',NULL,NULL),(4,'12','1231231231','123123','3123','2023-07-12 16:11:14',NULL,NULL),(5,'Eddy','hello@eddychang.me','eddy123','123123','2023-07-12 16:26:12',NULL,NULL),(6,'金妮4444','ginny@test.com','ginny13','12345','2023-07-25 15:40:15',NULL,NULL),(7,'xx','xxf@test.com','giny','123','2023-08-04 16:29:54',NULL,NULL),(8,'SI YI','yisichoco@gmail.com',NULL,NULL,'2023-08-07 16:21:14','109150685961710971645','https://lh3.googleusercontent.com/a/AAcHTtdSXCswzIOoPEuOP_k6qaUT6LtSsf0mR-Gted_nuvwm=s96-c');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +47,4 @@ INSERT INTO `users` VALUES (1,'金妮','ginny@test.com','ginny132','12345','2023
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-07 17:19:27
+-- Dump completed on 2023-08-12 17:44:24
