4dfaa2eafeba0fcd5f0ef0091e3066af2a801f40
eyesofkids <eddy@joomla.com.tw>
2023-10-29 18:54:01 +0800

    merge json-tool.js +a

--- utils/tool.js
+++ utils/tool.js
@@ -7,6 +7,16 @@ import dotenv from 'dotenv'
 
 import { readFile, writeFile } from 'fs/promises'
 
+// A helper function to assert the request ID param is valid
+// and convert it to a number (since it comes as a string by default)
+export function getIdParam(req) {
+  const id = req.params.id
+  if (/^\d+$/.test(id)) {
+    return Number.parseInt(id, 10)
+  }
+  throw new TypeError(`Invalid ':id' param: "${id}"`)
+}
+
 /**
  * Fetching data from the JSON file and parse to JS data
  * @param {string} pathname
