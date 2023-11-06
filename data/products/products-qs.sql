4ad04a0b108cc67a0b8553e30e8beb0062274c0c
eyesofkids <eddy@joomla.com.tw>
2023-11-06 13:15:35 +0800

    U

--- data/products/products-qs.sql
+++ data/products/products-qs.sql
@@ -1,6 +1,21 @@
 SELECT *
 FROM product
 WHERE (name LIKE '%e%')
+    AND (brand_id IN (1, 2))
+    AND (cat_id IN (4, 5, 6, 7, 8))
+    AND CONCAT(",", color, ",") REGEXP ",(1|2),"
+    AND CONCAT(",", size, ",") REGEXP ",(1|2),"
+    AND CONCAT(",", tag, ",") REGEXP ",(3|4),"
+    AND (
+        price BETWEEN 1500 AND 10000
+    )
+ORDER BY id asc
+LIMIT 10 OFFSET 0
+
+-- find_in_set
+SELECT *
+FROM product
+WHERE (name LIKE '%e%')
     AND (brand_id IN (1,2))
     AND (cat_id IN (4, 5, 6, 7, 8))
     AND (
--- routes/products.js
+++ routes/products.js
@@ -13,102 +13,181 @@ import {
 // 資料庫使用
 import sequelize from '#configs/db.js'
 const { Product } = sequelize.models
-import { QueryTypes } from 'sequelize'
+import { QueryTypes, Op } from 'sequelize'
 
-// import { readJsonFile } from '#utils/tool.js'
-router.get('/qs-swr-total', async (req, res, next) => {
+router.get('/qs-sq', async (req, res, next) => {
   // 獲取網頁的搜尋字串
   const {
-    page,
-    keyword,
-    brand_ids,
-    cat_ids,
-    colors,
-    tags,
-    sizes,
-    orderby,
-    perpage,
-    price_range,
+    page = 1,
+    keyword = '',
+    brand_ids = '',
+    cat_ids = '',
+    colors = '',
+    tags = '',
+    sizes = '',
+    orderby = '',
+    perpage = 10,
+    price_range = '',
   } = req.query
 
   // TODO: 這裡可以檢查各query string正確性或給預設值，檢查不足可能會產生查詢錯誤
 
-  // 建立資料庫搜尋條件
-  const conditions = []
+  const whereTmp = {}
 
-  // 關鍵字，keyword 使用 `name LIKE '%keyword%'`
-  conditions[0] = keyword ? `name LIKE '%${keyword}%'` : ''
+  if (keyword) {
+    whereTmp.name = {
+      [Op.like]: `%${keyword}%`,
+    }
+  }
 
-  // 品牌，brand_ids 使用 `brand_id IN (4,5,6,7)`
-  conditions[1] = brand_ids ? `brand_id IN (${brand_ids})` : ''
+  if (brand_ids) {
+    whereTmp.brand_id = brand_ids.split(',').map((v) => Number(v))
+  }
 
-  // 分類，cat_ids 使用 `cat_id IN (1, 2, 3, 4, 5)`
-  conditions[2] = cat_ids ? `cat_id IN (${cat_ids})` : ''
+  if (cat_ids) {
+    whereTmp.cat_id = cat_ids.split(',').map((v) => Number(v))
+  }
 
-  // 顏色: FIND_IN_SET(1, color) OR FIND_IN_SET(2, color)
-  conditions[3] = getFindInSet(colors, 'color')
+  const andList = []
 
-  // 標籤: FIND_IN_SET(3, tag) OR FIND_IN_SET(2, tag)
-  conditions[4] = getFindInSet(tags, 'tag')
+  const genConcatRegexp = (param, column) => {
+    // CONCAT(",", color, ",") REGEXP ",(1|2),"
+    const value = `,(${param.split(',').join('|')}),`
+    return sequelize.where(
+      sequelize.fn('CONCAT', ',', sequelize.col(column), ','),
+      {
+        [Op.regexp]: value,
+      }
+    )
+  }
 
-  // 尺寸: FIND_IN_SET(3, size) OR FIND_IN_SET(2, size)
-  conditions[5] = getFindInSet(sizes, 'size')
+  if (colors) {
+    andList.push(genConcatRegexp(colors, 'color'))
+  }
 
-  // 價格
-  conditions[6] = getBetween(price_range, 'price', 1500, 10000)
+  if (sizes) {
+    andList.push(genConcatRegexp(sizes, 'size'))
+  }
 
-  // 各條件為AND相接(不存在時不加入where從句中)
-  const where = getWhere(conditions, 'AND')
+  if (tags) {
+    andList.push(genConcatRegexp(tags, 'tag'))
+  }
 
-  // 排序用，預設使用id, asc
-  const order = getOrder(orderby)
+  if (price_range) {
+    const ranges = price_range.split(',')
 
-  // 分頁用
-  // page預設為1，perpage預設為10
-  const perpageNow = Number(perpage) || 10
-  const pageNow = Number(page) || 1
-  const limit = perpageNow
-  // page=1 offset=0; page=2 offset= perpage * 1; ...
-  const offset = (pageNow - 1) * perpageNow
+    const minValue = Number(ranges[0]) // maybe NaN?
+    const maxValue = Number(ranges[1]) // maybe NaN?
 
-  const sqlProducts = `SELECT * FROM product ${where} ${order} LIMIT ${limit} OFFSET ${offset}`
-  const sqlCount = `SELECT COUNT(*) AS count FROM product ${where}`
+    whereTmp.price = {
+      [Op.between]: [minValue, maxValue],
+    }
+  }
 
-  //console.log(sqlProducts.bgWhite)
+  let order = [['id', 'ASC']]
+
+  if (orderby) {
+    order = [orderby.split(',')]
+  }
+
+  try {
+    const { count, rows } = await Product.findAndCountAll({
+      where: { ...whereTmp, [Op.and]: andList },
+      raw: true, // 只需要資料表中資料,
+      logging: (msg) => console.log(msg.bgWhite),
+      offset: (Number(page) - 1) * Number(perpage),
+      limit: Number(perpage),
+      order,
+    })
+
+    return res.json({
+      status: 'success',
+      data: {
+        total: count,
+        page: Number(page),
+        perpage: Number(perpage),
+        products: rows,
+      },
+    })
+  } catch (e) {
+    console.log(e)
+
+    return res.json({
+      status: 'error',
+      message: 'wrong query string',
+    })
+  }
+
+  // // 建立資料庫搜尋條件
+  // const conditions = []
+  // // 關鍵字，keyword 使用 `name LIKE '%keyword%'`
+  // conditions[0] = keyword ? `name LIKE '%${keyword}%'` : ''
+  // // 品牌，brand_ids 使用 `brand_id IN (4,5,6,7)`
+  // conditions[1] = brand_ids ? `brand_id IN (${brand_ids})` : ''
+  // // 分類，cat_ids 使用 `cat_id IN (1, 2, 3, 4, 5)`
+  // conditions[2] = cat_ids ? `cat_id IN (${cat_ids})` : ''
+  // // 顏色: FIND_IN_SET(1, color) OR FIND_IN_SET(2, color)
+  // conditions[3] = getFindInSet(colors, 'color')
+  // // 標籤: FIND_IN_SET(3, tag) OR FIND_IN_SET(2, tag)
+  // conditions[4] = getFindInSet(tags, 'tag')
+  // // 尺寸: FIND_IN_SET(3, size) OR FIND_IN_SET(2, size)
+  // conditions[5] = getFindInSet(sizes, 'size')
+  // // 價格
+  // conditions[6] = getBetween(price_range, 'price', 1500, 10000)
+
+  // // 各條件為AND相接(不存在時不加入where從句中)
+  // const where = getWhere(conditions, 'AND')
+
+  // // 排序用，預設使用id, asc
+  // const order = getOrder(orderby)
+
+  // // 分頁用
+  // // page預設為1，perpage=limit預設為10
+  // const pageNow = Number(page) || 1
+  // // 每頁數量
+  // const limit = Number(perpage) || 10
+  // // page=1 offset=0; page=2 offset= perpage * 1; ...
+  // const offset = (pageNow - 1) * limit
+
+  // const sqlProducts = `SELECT * FROM product ${where} ${order} LIMIT ${limit} OFFSET ${offset}`
+  // const sqlCount = `SELECT COUNT(*) AS count FROM product ${where}`
+
+  // console.log(sqlProducts.bgWhite)
 
   // const products = await sequelize.query(sqlProducts, {
   //   type: QueryTypes.SELECT, //執行為SELECT
   //   raw: true, // 只需要資料表中資料
   // })
 
-  const data = await sequelize.query(sqlCount, {
-    type: QueryTypes.SELECT, //執行為SELECT
-    raw: true, // 只需要資料表中資料
-    plain: true, // 只需一筆資料
-  })
+  // const productCount = await sequelize.query(sqlCount, {
+  //   type: QueryTypes.SELECT, //執行為SELECT
+  //   raw: true, // 只需要資料表中資料
+  //   plain: true, // 只需一筆資料
+  // })
 
-  // 查詢
-  // const total = await countWithQS(where)
-  // const products = await getProductsWithQS(where, order, limit, offset)
+  // // json回傳範例
+  // //
+  // // {
+  // //   total: 100,
+  // //   perpage: 10,
+  // //   page: 1,
+  // //   data:[
+  // //     {id:123, name:'',...},
+  // //     {id:123, name:'',...}
+  // //   ]
+  // // }
 
-  // json回傳範例
-  //
-  // {
-  //   total: 100,
-  //   perpage: 10,
-  //   page: 1,
-  //   data:[
-  //     {id:123, name:'',...},
-  //     {id:123, name:'',...}
-  //   ]
-  // }
-
-  const result = {
-    total: data.count,
-  }
-
-  res.json(result)
+  // res.json({
+  //   status: 'success',
+  //   data: {
+  //     total: productCount.count,
+  //     perpage: Number(perpage) || 10,
+  //     page: Number(page) || 1,
+  //     products,
+  //   },
+  // })
 })
+
 router.get('/qs-swr', async (req, res, next) => {
   // 獲取網頁的搜尋字串
   const {
@@ -128,25 +207,18 @@ router.get('/qs-swr', async (req, res, next) => {
 
   // 建立資料庫搜尋條件
   const conditions = []
-
   // 關鍵字，keyword 使用 `name LIKE '%keyword%'`
   conditions[0] = keyword ? `name LIKE '%${keyword}%'` : ''
-
   // 品牌，brand_ids 使用 `brand_id IN (4,5,6,7)`
   conditions[1] = brand_ids ? `brand_id IN (${brand_ids})` : ''
-
   // 分類，cat_ids 使用 `cat_id IN (1, 2, 3, 4, 5)`
   conditions[2] = cat_ids ? `cat_id IN (${cat_ids})` : ''
-
   // 顏色: FIND_IN_SET(1, color) OR FIND_IN_SET(2, color)
   conditions[3] = getFindInSet(colors, 'color')
-
   // 標籤: FIND_IN_SET(3, tag) OR FIND_IN_SET(2, tag)
   conditions[4] = getFindInSet(tags, 'tag')
-
   // 尺寸: FIND_IN_SET(3, size) OR FIND_IN_SET(2, size)
   conditions[5] = getFindInSet(sizes, 'size')
-
   // 價格
   conditions[6] = getBetween(price_range, 'price', 1500, 10000)
 
@@ -157,12 +229,12 @@ router.get('/qs-swr', async (req, res, next) => {
   const order = getOrder(orderby)
 
   // 分頁用
-  // page預設為1，perpage預設為10
-  const perpageNow = Number(perpage) || 10
+  // page預設為1，perpage=limit預設為10
   const pageNow = Number(page) || 1
-  const limit = perpageNow
+  // 每頁數量
+  const limit = Number(perpage) || 10
   // page=1 offset=0; page=2 offset= perpage * 1; ...
-  const offset = (pageNow - 1) * perpageNow
+  const offset = (pageNow - 1) * limit
 
   const sqlProducts = `SELECT * FROM product ${where} ${order} LIMIT ${limit} OFFSET ${offset}`
   const sqlCount = `SELECT COUNT(*) AS count FROM product ${where}`
@@ -174,15 +246,11 @@ router.get('/qs-swr', async (req, res, next) => {
     raw: true, // 只需要資料表中資料
   })
 
-  const data = await sequelize.query(sqlCount, {
+  const productCount = await sequelize.query(sqlCount, {
     type: QueryTypes.SELECT, //執行為SELECT
     raw: true, // 只需要資料表中資料
     plain: true, // 只需一筆資料
   })
-
-  // 查詢
-  // const total = await countWithQS(where)
-  // const products = await getProductsWithQS(where, order, limit, offset)
 
   // json回傳範例
   //
@@ -196,14 +264,15 @@ router.get('/qs-swr', async (req, res, next) => {
   //   ]
   // }
 
-  const result = {
-    total: data.count,
-    perpage: Number(perpage),
-    page: Number(page),
-    data: products,
-  }
-
-  res.json(products)
+  res.json({
+    status: 'success',
+    data: {
+      total: productCount.count,
+      perpage: Number(perpage) || 10,
+      page: Number(page) || 1,
+      products,
+    },
+  })
 })
 
 // 獲得所有資料，加入分頁與搜尋字串功能，單一資料表處理
