553ce7eb535da0e3e7a8b1016bbe7b110c76ab16
Eddy Chang <eddy@joomla.com.tw>
2023-11-06 17:41:20 +0800

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -115,7 +115,7 @@ router.get('/', async (req, res) => {
     const { count, rows } = await Product.findAndCountAll({
       where: { [Op.and]: conditions },
       raw: true, // 只需要資料表中資料,
-      logging: (msg) => console.log(msg.bgWhite),
+      // logging: (msg) => console.log(msg.bgWhite),
       offset,
       limit,
       order,
